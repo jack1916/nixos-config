9ead219b66d5b98936dc0cd9937e6b723f7e5160
Jack <jackholsgrove@gmail.com>
2026-03-01 23:20:50 +0000

    fix: get fzf working by enabling bash in home-manager

--- home.nix
+++ home.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [
@@ -13,46 +18,53 @@
   home.homeDirectory = "/home/jack";
   home.stateVersion = "25.05";
 
-  home.packages = with pkgs;[
-  ripgrep
-  fd
+  home.packages = with pkgs; [
+    ripgrep
+    fd
 
-  tmux
-  fzf
-  bat
+    tmux
+    fzf
+    bat
 
-  gcc
-  gnumake
+    gcc
+    gnumake
   ];
   programs.tmux = {
     enable = true;
     clock24 = true;
     keyMode = "vi";
     mouse = true;
-    
+
     # This sets the prefix to Ctrl+s
     prefix = "C-s";
-    
+
     extraConfig = ''
       # Unbind the default prefix
       unbind C-b
-      
+
       # Bind Ctrl+s as the new prefix
       bind C-s send-prefix
-      
+
       # Custom keybindings for pane navigation
       bind-key h select-pane -L
       bind-key j select-pane -D
       bind-key k select-pane -U
       bind-key l select-pane -R
-      
+
       # Status bar styling
       set -g status-style 'bg=#333333 fg=#5eacd3'
     '';
-  };  
+  };
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
-    defaultOptions = [ "--height 40%" "--layout=reverse" "--border" ];
+    defaultOptions = [
+      "--height 40%"
+      "--layout=reverse"
+      "--border"
+    ];
+  };
+  programs.bash = {
+    enable = true;
   };
 }
